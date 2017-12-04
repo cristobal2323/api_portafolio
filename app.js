2f52abd6658a3b481c2e21c5d2cc43434099e14e
Cristóbal Maturana <cri.maturana@gmail.com>
2017-12-04 15:33:16 -0300

    cors

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const hbs = require('express-handlebars')
 const app = express()
 const api = require('./routes')
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 app.engine('.hbs', hbs({
   defaultLayout: 'default',
   extname: '.hbs'
